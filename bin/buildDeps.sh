51becc89c99996f22d1c4ed5e3b27e6a6be63583
James Roper <james@jazzy.id.au>
2018-07-06 13:42:27 +1000

    Update MRSO commit to track

--- bin/buildDeps.sh
+++ bin/buildDeps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This should be set to the commit hash that is being tracked. Needed even if TRACKING_PR is set.
-TRACKING_COMMIT="faa2dde"
+TRACKING_COMMIT="60f67ef"
 # To track a particular pull request, put it's number here, otherwise comment it out.
 TRACKING_PR="64"
 
